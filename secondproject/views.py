ee9c7dc7501435deba8589c71eac5ca6007e5f09
Peter2220 <peter.24953@eng.modern-academy.edu.eg>
2023-03-28 16:15:01 +0200

    Add templates, context

--- secondproject/views.py
+++ secondproject/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World!")
+# def index(request):
+#     return HttpResponse("Hello World!")
 
-def test(request):
-    return HttpResponse("Test Page")
+# def test(request):
+#     return HttpResponse("Test Page")
+
+
+def index(request):
+    mycontext={"name": "Ramos", "Age": 25}
+    return render(request, 'secondpage/index.html', mycontext)
