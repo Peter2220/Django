17b998bb80aece7baef799ed89c9d2dea2097e53
Peter2220 <peter.24953@eng.modern-academy.edu.eg>
2023-03-28 22:30:52 +0200

    Update project structure

--- pages/apps.py
+++ pages/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SecondprojectConfig(AppConfig):
+class PagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'secondproject'
+    name = 'pages'
